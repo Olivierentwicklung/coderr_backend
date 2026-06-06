924988d605a5064be2b630c99592c982073e2a02
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-06-06 14:06:02 +0200

    Add users authentication app to settings and enhance CustomUser model with profile file

--- users_auth_app/apps.py
+++ users_auth_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class UsersAuthAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users_auth_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users_auth_app"
--- users_auth_app/models.py
+++ users_auth_app/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    CUSTOMER = "customer"
+    BUSINESS = "business"
+
+    USER_TYPE_CHOICES = [
+        (CUSTOMER, "Customer"),
+        (BUSINESS, "Business"),
+    ]
+
+    email = models.EmailField(unique=True)
+    type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+
+    profile_file = models.ForeignKey(
+        "uploads_app.FileUpload",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="profile_users",
+    )
+
+    def __str__(self):
+        return self.username
