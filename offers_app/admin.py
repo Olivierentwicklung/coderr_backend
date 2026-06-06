180f36e96067fe1c1c993e8f7d7c5008f8b787e4
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-06-06 18:57:56 +0200

    Register models in admin panel for offers, orders, reviews, uploads, and users

--- offers_app/admin.py
+++ offers_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Offer, OfferDetail, OfferDetailFeature
+
 # Register your models here.
+admin.site.register(Offer)
+admin.site.register(OfferDetail)
+admin.site.register(OfferDetailFeature)
