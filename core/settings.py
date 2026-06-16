520c56b39f7eddc3fe41407fa04b29de1e95989e
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-06-16 21:23:36 +0200

    Add Dockerfile and .dockerignore; update ALLOWED_HOSTS in settings.py

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+# Liest die Hosts aus Docker, standardmäßig 'localhost' und '127.0.0.1'
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
